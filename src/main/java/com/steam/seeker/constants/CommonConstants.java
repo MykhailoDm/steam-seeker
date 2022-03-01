a891098f82b04c6b0f170652412d2962accd8097
MykhailoDm <mikhailo2002dm@gmail.com>
2022-03-01 21:50:02 +0200

    Added common constants

---
+++ src/main/java/com/steam/seeker/constants/CommonConstants.java
@@ -0,0 +1,8 @@
+package com.steam.seeker.constants;
+
+public final class CommonConstants {
+    private CommonConstants() {}
+
+    public static final String FORMAT = "format";
+    public static final String JSON = "json";
+}
